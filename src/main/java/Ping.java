d0809ed7c8cbb55a5e47faa7684fe327f6c0ddec
houkp <17611169761@163.com>
2019-05-21 15:28:11 +0800

    log日志

--- src/main/java/Ping.java
+++ src/main/java/Ping.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,10 +9,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Ping {
+
+    private static Logger logger = LoggerFactory.getLogger(Ping.class);
+
+
     public static void main(String[] args) {
-        String osName = System.getProperty("os.name");
-        System.out.println(osName);
-        boolean ping = ping("39.105.135.247", 0, 0);
+        boolean ping = ping("121.69.49.235", 1, 5000);
         System.out.println(ping);
     }
 
@@ -22,31 +27,33 @@ public class Ping {
     public static boolean ping(String ipAddress, int pingTimes, int timeOut) {
         BufferedReader in = null;
         Runtime r = Runtime.getRuntime();
-        // 将要执行的ping命令,此命令是windows格式的命令
-//        String pingCommand = "ping " + ipAddress + " -n " + pingTimes    + " -w " + timeOut;
-        String pingCommand = "ping " + ipAddress;
-        // Linux命令如下
-        // String pingCommand = "ping" -c " + pingTimes + " -w " + timeOut + ipAddress;
+        String osName = System.getProperty("os.name");
+        String pingCommand = null;
+        if (osName.toLowerCase().contains("windows")) {
+            // 将要执行的ping命令,此命令是windows格式的命令  -n多少次  -w 多少毫秒
+            pingCommand = "ping " + ipAddress + " -n " + pingTimes + " -w " + timeOut;
+        } else {
+            int second = timeOut / 1000;
+            // Linux命令如下  -c多少次 -w多少秒
+            pingCommand = "ping - c " + pingTimes + " - w " + second + ipAddress;
+        }
+
+
         try {
-            System.out.println(pingCommand);
+            logger.debug("pingCommand:{}", pingCommand);
             // 执行命令并获取输出
             Process p = r.exec(pingCommand);
             if (p == null) {
                 return false;
             }
-            in = new BufferedReader(new InputStreamReader(p.getInputStream(),"gbk"));
+
+            in = new BufferedReader(new InputStreamReader(p.getInputStream(), "gbk"));
             int connectedCount = 0;
             String line = null;
-            StringBuffer sb = new StringBuffer();
-            while ((line = in.readLine()) != null) {
-                sb.append(line);
-            }
-            System.out.println("返回值为:" + sb);
             // 逐行检查输出,计算类似出现=23ms TTL=62字样的次数
             while ((line = in.readLine()) != null) {
-                System.out.println(line);
+                logger.debug("line:{}", line);
                 connectedCount += getCheckResult(line);
-
             }
             // 如果出现类似=23ms TTL=62这样的字样,出现的次数=测试次数则返回真
             return connectedCount == pingTimes;
